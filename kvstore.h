b1401f1f20afa3ef68350a649aaf8a5cb92d5ee5
GDYYYY <48650628+GDYYYY@users.noreply.github.com>
2020-04-17 00:31:22 +0800

    Update kvstore.h

--- kvstore.h
+++ kvstore.h
@@ -55,7 +55,8 @@ struct buf_node
 {
     uint64_t key = 0;
     //string val = "";
-    int offset=0;
+    uint64_t offset=0;
+    buf_node* next=nullptr;
 };
 
 struct buffer
@@ -72,7 +73,7 @@ struct block_node
 {
     uint64_t key=0;
     string val="";
-    int offset=0;
+    //uint64_t offset=0;//需要吗？
     block_node* next=nullptr;
     block_node* last=nullptr;
 };
@@ -81,6 +82,7 @@ struct block
 {
     uint64_t min=0;
     uint64_t max=0;
+    uint64_t size=0;
     int level=0;
     int num=0;
     block_node *head=nullptr;//head有值
@@ -96,38 +98,62 @@ class KVStore : public KVStoreAPI
     // You can add your implementation here
 private:
     SkipList memtable;
-    buffer* offset_head=new buffer;
-    block* cur_block=nullptr;
-    nm num[20];
+    buffer* offset_head=new buffer;//offset_head无值
+    block* cur_block=nullptr; //用于归并
+    block* new_block=nullptr; //用于从文件中加载
+    nm *num;
 
 public:
-    // constexpr static int MAX = 2 * 1024 * 1024 / sizeof(buf_node);
-    // constexpr static int BLOCKSIZE = MAX * (sizeof(buf_node) + sizeof(buf_offset));
 
-    // constexpr static off_t M = (BLOCKSIZE - BGSIZE) / sizeof(Node)-1;
-    // constexpr static off_t L = (BLOCKSIZE - BGSIZE) / (Keysize + Valsize);
-
+    uint64_t cur_offset=0;
+    uint64_t cur_num=0;
+    uint64_t cur_len=0;
+    uint64_t cur_level=0;
+    bool isEnd=false;//记录查找到的位置
     KVStore(const std::string &dir);
 
     ~KVStore();
-    void load_offset();//加载索引（未写）
-    void mem_to_ss(SkipList memtable);
-     //memtable转换为sstable,第一个数据是size（用于跳转至索引区）（未写完）
-    void check_merge(int level);//检查该层是否需要下移（未写完）
-    void merge_ss(block* x);//将上层的block与下面一层合并 记得update_buffer(待debug)
+
+    //debug
+    void load_offset();//加载索引
+
+    //debug
+    bool search_from_offset(uint64_t key);//从offset中寻找是否存在
+
+    //debug
+    string get_val(uint64_t level,uint64_t num,uint64_t offset,uint64_t len);//获取值
+
+    //debug
+    void mem_to_ss(SkipList memtable);//debug
+     //memtable转换为sstable,第一个数据是size（用于跳转至索引区）
+
+     //debug
+    void check_merge(int level);//检查该层是否需要下移
+
+    //debug
+    void merge_ss(block* x);//将上层的block与下面一层合并 记得update_buffer
+
+    //debug
     block* load_block(int level,int num);//将文件转换为block
+
+    //debug
     void block_to_ss(block* x);//将block写为文件
-    // void merge_level0();
-    block* merge_block(block* x1,block* x2);//x1比x2新(未写完)
+ 
+    //debug
+    block* merge_block(block* x1,block* x2);//x1比x2新
     //合并两个block,得到的新block一个存在x2(不是文件），一个返回(实际是存在cur_block中）继续合并
-    void update_buffer(block* x);//更新buffer区域的buf_node，可以考虑将归并时的offset更新和max，min放在这里
+    
+    //debug
+    void update_buffer(block* x);//(debug)
+    //更新buffer区域的buf_node，可以考虑将归并时的offset更新和max，min放在这里
+    
     void put(uint64_t key, const std::string &s) override;
 
     std::string get(uint64_t key) override;
 
     bool del(uint64_t key) override;
 
-    void reset() override;
+    void reset() override;//未写
 };
 
 #endif
