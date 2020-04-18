8baea67c13cdd248fe52090681d26121d1892151
GDYYYY <48650628+GDYYYY@users.noreply.github.com>
2020-04-18 23:36:48 +0800

    Update kvstore.h

--- kvstore.h
+++ kvstore.h
@@ -5,7 +5,7 @@
 #include <ctime>
 using namespace std;
 //SkipList部分
-const int max_size = (2 * 1024 * 1024) ; 
+const int max_size = 2*1024*1024;//(2 * 1024 * 1024) ; 
 struct node
 {
     node *up;
@@ -16,6 +16,10 @@ struct node
     string val;
     node(uint64_t key, string val) : key(key), val(val), up(nullptr), down(nullptr), left(nullptr), right(nullptr) {}
 };//跳转表的节点
+// struct isDel{
+//     uint64_t key;
+//     isDel* next=nullptr;
+// };
 struct node_head
 {
     node_head *up;
@@ -29,16 +33,16 @@ private:
 public:
     node_head *bottom_head;
     node_head *top_head;
-    node *cur_node;
+    node *cur_node=nullptr;
     int tot_level = 0;
-    int size = 0;
-    int max = (2 * 1024 * 1024) ;
+    uint64_t size = 0;
+    int max = max_size;//(2 * 1024 * 1024) ;
 
-    SkipList(/* args */);
+    SkipList( );
     ~SkipList();
     bool ifup()
     {
-        srand(time(NULL));
+        srand(22);//(time(NULL));
         return (rand() % 2) & 1;
     };
     void put(uint64_t key, const std::string &s);
@@ -99,9 +103,11 @@ class KVStore : public KVStoreAPI
 private:
     SkipList memtable;
     buffer* offset_head=new buffer;//offset_head无值
-    block* cur_block=nullptr; //用于归并
-    block* new_block=nullptr; //用于从文件中加载
+    //block* cur_block=nullptr; //用于归并
+    //block* cur_block1=nullptr;
+    //block* new_block=nullptr; //用于从文件中加载
     nm *num;
+    
 
 public:
 
@@ -114,7 +120,7 @@ public:
 
     ~KVStore();
 
-    //debug
+    //应该没问题
     void load_offset();//加载索引
 
     //debug
@@ -123,30 +129,36 @@ public:
     //debug
     string get_val(uint64_t level,uint64_t num,uint64_t offset,uint64_t len);//获取值
 
-    //debug
-    void mem_to_ss(SkipList memtable);//debug
+    //应该没问题
+    void mem_to_ss(SkipList memtable);
      //memtable转换为sstable,第一个数据是size（用于跳转至索引区）
 
-     //debug
+     //debug 暂时问题出在load_block，merge_ss
     void check_merge(int level);//检查该层是否需要下移
 
     //debug
     void merge_ss(block* x);//将上层的block与下面一层合并 记得update_buffer
 
-    //debug
-    block* load_block(int level,int num);//将文件转换为block
+    //应该没问题(鬼)
+    block* load_block(int level,int num,block* x);//将文件转换为block x
 
-    //debug
+    //应该没问题
     void block_to_ss(block* x);//将block写为文件
  
-    //debug
-    block* merge_block(block* x1,block* x2);//x1比x2新
-    //合并两个block,得到的新block一个存在x2(不是文件），一个返回(实际是存在cur_block中）继续合并
+    //可能没问题
+    void merge_block(block* x1,block* x2,block* x3,block* x4);//x1比x2新
+    //合并两个block,得到的新block一个存在x3，一个返回(实际是存在cur_block中）继续合并
     
-    //debug
+    //应该没问题
     void update_buffer(block* x);//(debug)
     //更新buffer区域的buf_node，可以考虑将归并时的offset更新和max，min放在这里
     
+    void del_buffer(int level,int num);
+
+    void reset_block(block* x);
+
+    void checkNum(uint64_t level);
+
     void put(uint64_t key, const std::string &s) override;
 
     std::string get(uint64_t key) override;
